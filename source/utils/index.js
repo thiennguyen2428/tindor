dfcb4b726a15dc76afe74e23e5ab4b8affdd5af8
Nguyễn Châu Thành Thiện <thiensmail96@gmail.com>
2018-11-22 10:30:31 +0700

    Add common Constant and Styles

--- source/utils/index.js
+++ source/utils/index.js
@@ -1,5 +1,18 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
 
+// Constant
 export const SCREEN_WIDTH = Dimensions.get('window').width;
 export const SCREEN_HEIGHT = Dimensions.get('window').height;
 export const TAB_BAR_HEIGHT = 50;
+
+// Style
+export const SHADOW_STYLE = Platform.select({
+  ios: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.2,
+  },
+  android: {
+    elevation: 5,
+  },
+});
